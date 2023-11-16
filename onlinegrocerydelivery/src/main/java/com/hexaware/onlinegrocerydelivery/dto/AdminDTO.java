9d17245310050455327de910fa93e71399074557
saisakitha <150312213+saisakitha@users.noreply.github.com>
2023-11-16 09:02:25 +0530

    Validation

--- onlinegrocerydelivery/src/main/java/com/hexaware/onlinegrocerydelivery/dto/AdminDTO.java
+++ onlinegrocerydelivery/src/main/java/com/hexaware/onlinegrocerydelivery/dto/AdminDTO.java
@@ -1,12 +1,21 @@
 package com.hexaware.onlinegrocerydelivery.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public class AdminDTO {
 	private int adminId;
-	@NotBlank(message="should not be null")
+	@NotBlank(message="Username should not be blank")
+	@Size(min=8,max=20,message="Username should be between 8-20 characters")
 	private String userName;
+	@NotBlank(message = "Password should not be blank")
+    @Size(min=6,message="Password must be at least 8 characters long")
+   /* @Pattern.List({
+        @Pattern(regexp = "(?=.*[0-9]).+", message = "Password must contain at least one digit"),
+        @Pattern(regexp = "(?=.*[a-z]).+", message = "Password must contain at least one lowercase letter"),
+        @Pattern(regexp = "(?=.*[A-Z]).+", message = "Password must contain at least one uppercase letter"),
+        @Pattern(regexp = "(?=.*[!@#$%^&*()\\-_=+{};:',.<>?/]).+", message = "Password must contain at least one special character")
+    })*/
 	private String password;
 
 	
